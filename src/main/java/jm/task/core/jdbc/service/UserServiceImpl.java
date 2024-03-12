735c6af2558521c5f49d0c9d2a24ff704f61d171
Rarka <90555389+Rarka1-01@users.noreply.github.com>
2024-03-12 23:25:31 +0700

    Add annotation override

--- src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
+++ src/main/java/jm/task/core/jdbc/service/UserServiceImpl.java
@@ -15,26 +15,32 @@ public class UserServiceImpl implements UserService {
         uD = new UserDaoHibernateImpl();
     }
 
+    @Override
     public void createUsersTable() {
         uD.createUsersTable();
     }
 
+    @Override
     public void dropUsersTable() {
         uD.dropUsersTable();
     }
 
+    @Override
     public void saveUser(String name, String lastName, byte age) {
         uD.saveUser(name, lastName, age);
     }
 
+    @Override
     public void removeUserById(long id) {
         uD.removeUserById(id);
     }
 
+    @Override
     public List<User> getAllUsers() {
         return uD.getAllUsers();
     }
 
+    @Override
     public void cleanUsersTable() {
         uD.cleanUsersTable();
     }
